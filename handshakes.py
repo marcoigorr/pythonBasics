3e5ccbcb8b31924623fbe2f04038ff19e3d05a41
marcoigorr <petronio.mrc@gmail.com>
2022-12-01 13:49:45 +0100

    + handshakes.py working

--- handshakes.py
+++ handshakes.py
@@ -1,12 +1,14 @@
 
-def print_handshakes(people):
-    handshakes = {}
+def print_handshakes(people) -> int:
+    length = len(people)
+    nHandshakes = 0
+    strHandshakes = ''
 
-    for i in range(len(people)):
-        for j in range(i, len(people) - 1):
-            if people[i] not in handshakes:
-                handshakes[people[i]] = people[j + 1]
-            else:
-                handshakes[people[i]] += ',', people[j + 1]
+    for i in range(length - 1):
+        for j in range(i + 1, length):
+            nHandshakes += 1
+            strHandshakes += f"\n{people[i]} shakes hand with {people[j]}"
 
-    return len(handshakes.values())
+    print(f"Handshakes: {strHandshakes} \nCount: {nHandshakes}\n")
+
+    return nHandshakes
